9aaa9938214da827d829a5428bf8095263c7b29e
alexfalconflores <alexfalconflores@hotmail.com>
2023-10-19 23:25:06 -0500

    [ADD] Category and Task Model

---
+++ project-ef/Models/Category.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace project_ef.Models;
+
+public class Category
+{
+    //[Key]
+    public Guid Id { get; set; }
+    //[Required]
+    //[MaxLength(250)]
+	public string Name { get; set; }
+    public string Description { get; set; }
+    public int Weight { get; set; }
+
+    [JsonIgnore]
+    public virtual ICollection<Task> Tasks { get; set; }
+
+    public Category()
+	{
+	}
+}
+
+public static class CategoryFluent
+{
+    public static Category Id(this Category category,string guid)
+    {
+        category.Id = Guid.Parse(guid);
+        return category;
+    }
+
+    public static Category Name(this Category category, string name)
+    {
+        category.Name = name;
+        return category;
+    }
+
+    public static Category Description(this Category category, string description)
+    {
+        category.Description = description;
+        return category;
+    }
+
+    public static Category Weight(this Category category, int weight)
+    {
+        category.Weight = weight;
+        return category;
+    }
+}
---
+++ project-ef/Models/Task.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace project_ef.Models;
+
+public class Task
+{
+	//[Key]
+	public Guid Id { get; set; }
+	//[ForeignKey("CategoryId")]
+	public Guid CategoryId { get; set; }
+	//[Required]
+	//[MaxLength(250)]
+	public string Title { get; set; }
+	public string Description { get; set; }
+	public Priority TaskPriority { get; set; }
+    public DateTime CreationDate { get; set; }
+    public virtual Category Category { get; set; }
+	//[NotMapped]
+	public string Summary { get; set; }
+
+	public Task()
+	{
+	}
+}
+
+public enum Priority
+{
+	low,
+	Medium,
+	High
+}
+
+public static class TaskFluent
+{
+    public static Task Id(this Task task, string guid)
+    {
+        task.Id = Guid.Parse(guid);
+        return task;
+    }
+
+    public static Task CategoryId(this Task task, string guid)
+    {
+        task.CategoryId = Guid.Parse(guid);
+        return task;
+    }
+
+    public static Task Title(this Task task, string title)
+    {
+        task.Title = title;
+        return task;
+    }
+
+    public static Task Description(this Task task, string description)
+    {
+        task.Description = description;
+        return task;
+    }
+
+    public static Task TaskPriority(this Task task, Priority taskPriority)
+    {
+        task.TaskPriority = taskPriority;
+        return task;
+    }
+
+    public static Task CreationDate(this Task task, DateTime creationDate)
+    {
+        task.CreationDate = creationDate;
+        return task;
+    }
+
+    //public static Task CreationDate(this Task task, DateTimeOffset creationDate)
+    //{
+    //    task.CreationDate = DateTimeOffset.Now;
+    //}
+}
+
